3f226df824aa05cf8d6197e226cbe863128728ba
kimberlycbrown <kbrown789@gmail.com>
2017-06-23 18:12:19 +0000

    Added comments

--- PCA.R
+++ PCA.R
@@ -26,20 +26,25 @@ Correlated_stocks <- function(){
   var.env$unc_res <- list()
   var.env$unc_R2 <- list()
   
+  #Loops through each stock in Stock_Return
   for (j in 1:491){
   
     stock <- sort(PCA_correlation[,j], decreasing=TRUE)[1:num_stocks]
     corr_stock <- names(stock)
     
+    #gets names of top 50 correlated stocks
     string <- ""
     for (i in 2:49){
       string <- paste0(string,corr_stock[i],"+")
     }
     string <- paste0(string,corr_stock[50])
     
+    #Finds best fit using stepwise regression
     command_string <- paste0("fit <- lm(",stocks[j],"~0+",string,",data=var.env$Stock_Return)")
     eval(parse(text=command_string))
-    step <- stepAIC(fit, direction="both")
+    step <- stepAIC(fit, direction="both",trace=FALSE)
+    
+    #Stores results in unc_res and unc_R2
     result <- step$coefficients
     var.env$unc_res[[j]] <- result
     names(var.env$unc_res)[j] <- stocks[j]
@@ -50,43 +55,62 @@ Correlated_stocks <- function(){
 
 Pos_Coff_Correlation <- function(){
   PCA_correlation <- cor(var.env$Stock_Return)
-  j<-1
+  j <- 1
   num_stocks <- length(var.env$unc_res[[j]])
+  
   lower_bound <- as.list(rep(0, num_stocks))
+  
+  #sets initial variable values equal to values found in the unconstrained model and converts any negative values to zero
   initial_value <- var.env$unc_res[[j]]
   initial_value[initial_value<0] <- 0
+  for (i in 1:num_stocks){
+    names(initial_value[i]) <- unlist(strsplit(names(step$coefficients)[i],"[.]"))[1]
+  }
+  
   corr_stock <- names(var.env$unc_res[[j]])
   string <- ""
-  for (i in 1:(num_stocks-1)){
-    string <- paste0(string,corr_stock[i],"+")
+  for (i in 1:num_stocks){
+    string <- paste0(string,"+",corr_stock[i])
   }
   string <- paste0(string,corr_stock[num_stocks])
   
   stock_return = var.env$Stock_Return[,j]
   var.env$Stock_Return.df <- as.data.frame(var.env$Stock_Return)
-  for (k in 1:length(var.env$Stock_Return.df)){
-    if (names(var.env$Stock_Return)[k] %in% corr_stock){
-      stock_return <- data.frame(stock_return,var.env$Stock_Return[,k])
-    }
-  }
-  command_string <- paste0("fit <- nls(",stocks[1],"~0+",string,",data=stock_return,start=initial_value,lower=lower_bound)")
+  
+  string <- "A.Adjusted ~ beta1*FOX.Adjusted + beta2*FOXA.Adjusted + beta3*CSCO.Adjusted + beta4*PX.Adjusted + beta5*FMC.Adjusted + beta6*ROK.Adjusted + beta7*MOLX.Adjusted + beta8* CMI.Adjusted + beta9*ETN.Adjusted + beta10*BEN.Adjusted + beta19*RRD.Adjusted + beta11*WMB.Adjusted + beta12*PH.Adjusted + beta13*INTC.Adjusted + beta14*MMM.Adjusted + beta15*TMO.Adjusted + beta16*BMS.Adjusted + beta17*JEC.Adjusted + beta18*TER.Adjusted"
+  
+  command_string <- paste0("fit <- nls(",stocks[j],"~0",string,",data=var.env$Stock_Return.df,alg=\"port\",start=initial_value,lower=lower_bound)")
   eval(parse(text=command_string))
-}
+
+  g <- function(x)
+    function(beta1,beta2,beta3)
+      beta1+ beta2*x+beta3*x^2
+  
+  out <- nls(y~g(x)(beta1,beta2,beta3),data=data.frame(x,y),
+             alg="port",
+             start=list(beta1=1,beta2=1,beta3=1),
+             lower=list(beta1=1,beta2=1,beta3=1))
+  }
 
 PCA_Analysis <- function(){
-  #june 2006 - May 2007 PCA analysis
+  #June 2006 - May 2007 PCA analysis
   pr.out<-prcomp(var.env$Stock_Return[104:354], center=TRUE, scale=TRUE)
+  
+  #Matrix of PCA components
   var.env$eig.array <- pr.out$rotation
+  
+  #Top 10 and Bottom 10 stocks for 2-6 PCA components
   var.env$top10 <- list()
   var.env$bottom10 <- list()
   for (i in 2:6){
-    sorted <- sort(var.env$eig.array[,i], decreasing=TRUE)
+    sorted <- sort(var.env$eig.array[,1], decreasing=TRUE)
     var.env$top10[[i-1]] <- names(sorted[1:10])
     var.env$bottom10[[i-1]] <- names(tail(sorted, 10))
   }
   names(var.env$top10) <- names(var.env$eig.array[1,][2:6])
   names(var.env$bottom10) <- names(var.env$eig.array[1,][2:6])
 
+  #Plot of explained variance
   pr.var<-pr.out$sdev^2
   pve<-pr.var/sum(pr.var)
   plot(pve, xlab="Principal component", ylab="Proportion of Variance Explained", ylim=c(0,1), type='b')
--- main.R
+++ main.R
@@ -4,7 +4,8 @@ print(paste("Start time:",Sys.time()))
 if (!exists("stx_list.loaded")) stx_list.loaded <- NULL 
 rm(list = ls(all=TRUE)[!ls(all=TRUE) %in% c("data.env","stx_list.loaded")]) #clean workspace except for (data.env, stx.list.old) so we don't have to reload data
 
-source("init.R")            
+source("init.R")     
+set_up_environments()
 load_custom_libraries()
 load_packages()
 stock_list_PCA()   
@@ -12,7 +13,6 @@ stx_list.loaded <- load_stock_history_PCA(stx_list.loaded)
 
 #code below should be set up as functions where possible
 Returns(stx_list.loaded)
-
 Correlated_stocks()
 Pos_Coff_Correlation()
 PCA_Analysis ()
